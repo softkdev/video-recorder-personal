42de573087b6fe0aaa9aba4333e8ca741f065939
m.kavand <kavand123456789@gmail.com>
2021-08-10 16:32:46 +0430

    new update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // export { CameraCheck } from "./component/CameraCheck.jsx";
 export { RecordVideo } from "./component/RecordVideo.jsx";
 // import "./style.css";
-import { Recorder } from "./Recorder";
-import { CameraCheck } from "./CameraCheck"; //return true or error message
+export { Recorder } from "./Recorder.jsx";
+export { CameraCheck } from "./CameraCheck.jsx"; //return true or error message
 
-export { CameraCheck, Recorder };
+// export { CameraCheck, Recorder };

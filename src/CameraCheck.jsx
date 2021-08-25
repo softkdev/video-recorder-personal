1bb76d176425df55adb01efb7d4c45cb37729d08
m.kavand <kavand123456789@gmail.com>
2021-08-25 11:55:10 +0430

    fix the cameracheck

--- src/CameraCheck.jsx
+++ src/CameraCheck.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { isMacOs, osName } from "react-device-detect";
 
 export const CameraCheck = () => {
-  let stateType = false;
+  const [stateType, setStateType] = useState(false);
   if (osName !== "iOS" && !isMacOs) {
     if (window.location.protocol === "https:") {
       navigator.getUserMedia =
@@ -16,12 +16,14 @@ export const CameraCheck = () => {
           "getUserMedia" in navigator.mediaDevices
         ) {
           navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+          console.log("convert true all media", navigator.mediaDevices);
         }
         navigator.getUserMedia(
           { audio: true, video: true },
           (stream) => {
+            console.log("strem existance", stateType);
             if (stateType) {
-              stateType = false;
+              setStateType(false);
               window.location.reload();
             }
             document.querySelector("video").src =
@@ -29,53 +31,47 @@ export const CameraCheck = () => {
             var mediaStreamTrack = stream.getVideoTracks()[0];
 
             if (typeof mediaStreamTrack != "undefined") {
+              console.log("mediaStreamTracker", mediaStreamTrack);
               mediaStreamTrack.onended = function () {
-                stateType = true;
                 return "دوربین شما مشغول است";
                 //for Chrome.
               };
             } else {
-              stateType = true;
               return "دسترسی دوربین در دستگاه شما غیر فعال است. لطفا آن را فعال کنید";
             }
+
+            return true;
           },
           (e) => {
+            console.log("error userMedia", e.name);
             switch (e.name) {
               case "NotFoundError":
-                stateType = true;
                 return "دوربین دستگاه شما پیدا نشد";
                 break;
               case "DevicesNotFoundError":
-                stateType = true;
                 return "دسترسی دوربین در دستگاه شما غیر فعال است. لطفا آن را فعال کنید";
                 break;
               case "SourceUnavailableError":
-                stateType = true;
                 return "دوربین شما مشغول است";
                 break;
               case "PermissionDeniedError":
-                stateType = true;
                 return "لطفا به مرورگر خود دسترسی دوربین را بدهید!";
                 break;
               case "SecurityError":
-                stateType = true;
                 return "دسترسی دوربین در دستگاه شما غیر فعال است. لطفا آن را فعال کنید";
                 break;
               default:
-                stateType = true;
                 return "دسترسی دوربین در دستگاه شما غیر فعال است. لطفا آن را فعال کنید";
             }
           }
         );
       } else {
-        stateType = true;
         return "لطفا دسترسی مرورگر به دوربین بررسی شود! در صورت دسترسی داشتن تایید نمایید";
       }
     } else {
-      stateType = true;
       return "شما در حال مشاهده سایت از یک پروتوکل نا امن هستید برخی از دسترسی هابرای شما غیر فعال شده است";
     }
+  } else {
     return true;
   }
-  return true;
 };
